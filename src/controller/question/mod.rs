1803a50e06dc17c080672db0ae8ad6b5ae255c19
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-08-18 00:13:50 +0800

    feat: add put question

--- src/controller/question/mod.rs
+++ src/controller/question/mod.rs
@@ -7,6 +7,6 @@ mod modify;
 
 pub fn get_question_routers() -> Router {
     Router::new()
-        .route("/", get(page::get_page).post(modify::new_question))
+        .route("/", get(page::get_page).post(modify::new_question).put(modify::modify_question))
         .route("/:question", get(question::get_question))
 }
--- src/controller/question/modify.rs
+++ src/controller/question/modify.rs
@@ -14,8 +14,28 @@ pub async fn new_question(Json(question): Json<NewQuestionRequest>) -> String {
     id.to_string()
 }
 
+pub async fn modify_question(Json(question): Json<ModifyQuestionRequest>) -> String {
+    let content = serde_json::to_value(question.content).unwrap();
+    let values = question.values.map(|values| 
+        values.iter().map(|v| serde_json::to_value(v).unwrap()).collect());
+    let condition = question.condition.map(|c| serde_json::to_string(&c).unwrap());
+    
+    let id = save_question(content, question.r#type, values, condition, question.required, Some(question.id.clone())).await;
+    id.to_string()
+}
+
 #[derive(serde::Deserialize, Serialize)]
 pub struct NewQuestionRequest {
+    pub content: ValueWithTitle,
+    pub r#type: QuestionType,
+    pub values: Option<Vec<ValueWithTitle>>,
+    pub condition: Option<Vec<Condition>>,
+    pub required: bool,
+}
+
+#[derive(serde::Deserialize, Serialize)]
+pub struct ModifyQuestionRequest {
+    pub id: String,
     pub content: ValueWithTitle,
     pub r#type: QuestionType,
     pub values: Option<Vec<ValueWithTitle>>,
