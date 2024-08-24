8d6b84d3c78d6cfb5c4269074700358a3de246e8
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-08-24 12:20:51 +0800

    fix: remove admin token cache

--- src/service/admin.rs
+++ src/service/admin.rs
@@ -3,18 +3,10 @@ use crate::model::generated::admin;
 use crate::model::generated::prelude::Admin;
 use axum::extract::FromRequestParts;
 use axum::http::request::Parts;
-use lazy_static::lazy_static;
 use migration::async_trait::async_trait;
-use moka::future::Cache;
 use sea_orm::ColumnTrait;
 use sea_orm::{EntityTrait, QueryFilter};
-use std::time::Duration;
-
-lazy_static! {
-    static ref ADMIN_TOKEN_CACHE: Cache<String, admin::Model> = Cache::builder()
-        .time_to_idle(Duration::from_secs(60 * 60 * 24 * 7)) //if the key is not accessed for 7 days, it will be removed
-        .build();
-}
+use crate::service::token::get_user_id;
 
 pub async fn get_admin_by_id(id: i32) -> Option<admin::Model> {
     Admin::find()
@@ -22,14 +14,6 @@ pub async fn get_admin_by_id(id: i32) -> Option<admin::Model> {
         .one(&*crate::DATABASE).await.unwrap()
 }
 
-pub async fn register_admin_token(token: &str, user: i32) {
-    let admin = get_admin_by_id(user).await.unwrap();
-    ADMIN_TOKEN_CACHE.insert(token.to_string(), admin).await;
-}
-
-pub async fn get_admin_by_token(token: &str) -> Option<admin::Model> {
-    ADMIN_TOKEN_CACHE.get(token).await
-}
 
 pub struct AdminTokenInfo(pub admin::Model);
 
@@ -39,22 +23,20 @@ where S: Send + Sync {
     type Rejection = ErrorMessage;
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        ADMIN_TOKEN_CACHE.insert("222".to_string(), admin::Model {
-            id: 222,
-            username: "222".to_string(),
-            disabled: false,
-        }).await;
         
         let headers = &parts.headers;
         let token = headers.get("token")
             .ok_or(ErrorMessage::InvalidToken)?
             .to_str()
             .map_err(|_| ErrorMessage::InvalidToken)?;
-        let user = get_admin_by_token(token).await
+        let user = get_user_id(token).await
+            .ok_or(ErrorMessage::TokenNotActivated)?;
+        
+        let user = get_admin_by_id(user.uid.parse().unwrap()).await
             .ok_or(ErrorMessage::TokenNotActivated)?;
         
         if user.disabled { 
-            return Err(ErrorMessage::TokenNotActivated);
+            return Err(ErrorMessage::InvalidToken);
         }
 
         Ok(AdminTokenInfo(user))
