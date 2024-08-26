7118bacd136f4d2213acea192e7d6366a3834b33
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-08-26 11:24:34 +0800

    fix: question struct

--- src/controller/question/modify.rs
+++ src/controller/question/modify.rs
@@ -6,22 +6,39 @@ use serde::Serialize;
 
 pub async fn new_question(Json(question): Json<NewQuestionRequest>) -> String {
     let content = serde_json::to_value(question.content).unwrap();
-    let values = question.values.map(|values| 
+    let values = question.values.map(|values|
         values.iter().map(|v| serde_json::to_value(v).unwrap()).collect());
     let condition = question.condition.map(|c| serde_json::to_string(&c).unwrap());
-    
+
+    let mut flag = true;
+    let mut answer = Some(Answer {
+        all_points: question.all_score.unwrap_or_else(|| {
+            flag = false;
+            0
+        }),
+        sub_points: question.sub_score,
+        answer: question.answer.unwrap_or_else(|| {
+            flag = false;
+            String::new()
+        }),
+    });
+
+    if !flag {
+        answer = None;
+    }
+
     let id = save_question(content, question.r#type, values, condition, question.required,
-                           None, question.answer).await;
+                           None, answer).await;
     id.to_string()
 }
 
 pub async fn modify_question(Json(question): Json<ModifyQuestionRequest>) -> String {
     let content = serde_json::to_value(question.content).unwrap();
-    let values = question.values.map(|values| 
+    let values = question.values.map(|values|
         values.iter().map(|v| serde_json::to_value(v).unwrap()).collect());
     let condition = question.condition.map(|c| serde_json::to_string(&c).unwrap());
-    
-    let id = save_question(content, question.r#type, values, condition, question.required, Some(question.id.clone()), 
+
+    let id = save_question(content, question.r#type, values, condition, question.required, Some(question.id.clone()),
                            question.answer).await;
     id.to_string()
 }
@@ -33,7 +50,9 @@ pub struct NewQuestionRequest {
     pub values: Option<Vec<ValueWithTitle>>,
     pub condition: Option<Vec<Condition>>,
     pub required: bool,
-    pub answer: Option<Answer>,
+    pub answer: Option<String>,
+    pub all_score: Option<i32>,
+    pub sub_score: Option<i32>,
 }
 
 #[derive(serde::Deserialize, Serialize)]
