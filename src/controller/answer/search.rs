d145c7b135a6ee40fd48ee3912d5284ae4bcb08a
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-08-30 00:52:12 +0800

    feat: add search

--- src/controller/answer/search.rs
+++ src/controller/answer/search.rs
@@ -5,8 +5,9 @@ use crate::DATABASE;
 use axum::extract::Query;
 use sea_orm::ColumnTrait;
 use sea_orm::{EntityTrait, PaginatorTrait, QueryFilter};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use tracing::info;
+use crate::model::generated::answer;
 
 pub async fn search_answer(AdminTokenInfo(admin): AdminTokenInfo, Query(request): Query<SearchAnswerQuery>) -> Result<String, ErrorMessage> {
     info!("Admin {} search answer", admin.id);
@@ -14,22 +15,37 @@ pub async fn search_answer(AdminTokenInfo(admin): AdminTokenInfo, Query(request)
     let mut answers = Answer::find();
 
     if let Some(survey) = request.survey {
-        answers = answers.filter(crate::model::generated::answer::Column::Survey.eq(survey));
+        answers = answers.filter(answer::Column::Survey.eq(survey));
     }
     if let Some(user) = request.user {
-        answers = answers.filter(crate::model::generated::answer::Column::User.eq(user));
+        answers = answers.filter(answer::Column::User.eq(user));
+    }
+    if let Some(true) = request.only_unfinished { 
+        answers = answers.filter(answer::Column::Completed.eq(false));
     }
     
-    let answers = answers.paginate(&*DATABASE, request.size.unwrap_or(10));
-    let answers = answers.fetch_page(request.page).await.unwrap();
+    let paginator = answers.paginate(&*DATABASE, request.size.unwrap_or(10));
+    let answers = paginator.fetch_page(request.page).await.unwrap();
+    
+    let answers = SearchAnswerResponse {
+        records: answers,
+        total: paginator.num_pages().await.unwrap(),
+    };
 
     Ok(serde_json::to_string(&answers).unwrap())
 }
 
 #[derive(Deserialize)]
 pub struct SearchAnswerQuery {
-    pub page: u64,
-    pub size: Option<u64>,
-    pub survey: Option<i32>,
-    pub user: Option<i64>,
+    page: u64,
+    size: Option<u64>,
+    survey: Option<i32>,
+    user: Option<i64>,
+    only_unfinished: Option<bool>,
+}
+
+#[derive(Serialize)]
+pub struct SearchAnswerResponse {
+    records: Vec<answer::Model>,
+    total: u64,
 }
