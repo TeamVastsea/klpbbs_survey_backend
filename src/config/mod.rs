bd9703e0913a710a87d837d15a88c000375f71af
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-10-24 17:52:37 +0800

    init

---
+++ src/config/mod.rs
@@ -0,0 +1,70 @@
+use std::fs::{create_dir_all, OpenOptions};
+use std::io::{Read, Write};
+
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+pub mod core;
+
+pub fn get_config<T>(name: &str) -> T
+where T: for<'a> Deserialize<'a> + Serialize
+{
+    let file_name = format!("config/{}.toml", name);
+    let mut raw_config = String::new();
+
+    create_dir_all("config").unwrap();
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(false)
+        .open(&file_name)
+        .unwrap_or_else(|_| panic!("Cannot open {}", &file_name));
+    file.read_to_string(&mut raw_config).unwrap();
+
+    let config: T = toml::from_str(&raw_config).unwrap();
+    save_changes(name, &raw_config, &config);
+    
+    info!("Config loaded: {}", &file_name);
+
+    config
+}
+
+pub fn save_changes<T>(name: &str, old_config: &str, config: &T)
+where T: Serialize
+{
+    let file_name = format!("config/{}.toml", name);
+    let new_config = toml::to_string_pretty(config).unwrap();
+
+    let mut document = String::new();
+
+    for diff in diff::lines(old_config, &new_config) {
+        match diff {
+            diff::Result::Left(l) => {
+                if l.trim_start().starts_with("#") || l.is_empty() {
+                    // keep comments or empty lines
+                    document.push_str(l);
+                    document.push('\n');
+                    continue;
+                }
+            },
+            diff::Result::Both(l, _) => {
+                document.push_str(l);
+                document.push('\n');
+                continue;
+            },
+            diff::Result::Right(r) => {
+                document.push_str(r);
+                document.push('\n');
+                continue;
+            },
+        }
+    }
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(&file_name)
+        .unwrap_or_else(|_| panic!("Cannot open {}", &file_name));
+    file.write_all(document.trim_end().as_bytes()).unwrap();
+}
