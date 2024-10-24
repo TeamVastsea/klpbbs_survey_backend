5f2812a5b15fa105a6cdc1d5d20ef65b2d6621b7
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-10-24 21:04:36 +0800

    feat: create page survey question table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,18 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
+mod m20241024_095446_create_survey_table;
+mod m20241024_100652_create_page_table;
+mod m20241024_101119_create_question_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        vec![
+            Box::new(m20241024_095446_create_survey_table::Migration),
+            Box::new(m20241024_100652_create_page_table::Migration),
+            Box::new(m20241024_101119_create_question_table::Migration),
+        ]
     }
 }
---
+++ migration/src/m20241024_100652_create_page_table.rs
@@ -0,0 +1,54 @@
+use sea_orm_migration::prelude::*;
+use sea_orm_migration::schema::{integer, pk_uuid, string};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Page::Table)
+                    .if_not_exists()
+                    .col(pk_uuid(Page::Id))
+                    .col(string(Page::Title))
+                    .col(integer(Page::Order).auto_increment())
+                    .col(integer(Page::Survey))
+                    .to_owned(),
+            )
+            .await?;
+        manager.create_foreign_key(
+            ForeignKey::create()
+                .name("fk_page_survey")
+                .from_tbl(Page::Table)
+                .from_col(Page::Survey)
+                .to_tbl(Survey::Table)
+                .to_col(Survey::Id)
+                .on_delete(ForeignKeyAction::Cascade)
+                .to_owned(),
+        ).await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Page::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Page {
+    Table,
+    Id,
+    Title,
+    Order,
+    Survey,
+}
+
+#[derive(DeriveIden)]
+enum Survey {
+    Table,
+    Id,
+}
