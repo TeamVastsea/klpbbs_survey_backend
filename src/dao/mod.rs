e17d1065ed39baee4b90fb331d3f37aeda3d1840
zrll_ <46812903+zrll12@users.noreply.github.com>
2025-01-29 23:29:01 +0800

    fix: time are marked as utc

--- src/dao/mod.rs
+++ src/dao/mod.rs
@@ -1,2 +1,29 @@
 pub mod entity;
 pub mod model;
+
+use sea_orm::prelude::DateTime;
+use serde::{Serializer, ser::Error, Deserialize};
+
+fn serialize_datetime_as_z<S>(dt: &DateTime, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let s = dt.to_string() + "Z";
+    serializer.serialize_str(&s)
+}
+
+fn serialize_option_datetime_as_z<S>(
+    dt: &Option<DateTime>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    match dt {
+        Some(date) => {
+            let s = date.to_string() + "Z";
+            serializer.serialize_str(&s)
+        }
+        None => serializer.serialize_none()
+    }
+}
