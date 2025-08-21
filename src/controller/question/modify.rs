43c531a83a92c818c33a6a5b63b39527a43b2fea
zrll <46812903+zrll12@users.noreply.github.com>
2025-08-21 17:58:07 +0800

    fix: add authenticate to modify question

--- src/controller/question/modify.rs
+++ src/controller/question/modify.rs
@@ -1,22 +1,27 @@
 use crate::controller::error::ErrorMessage;
 use crate::dao::model::question::{NewQuestion, Question};
+use crate::service::token::AdminTokenInfo;
 use axum::Json;
 use sea_orm::{ActiveModelTrait, IntoActiveModel};
 use serde::Deserialize;
+use tracing::info;
 
-pub async fn new_question(Json(question): Json<NewQuestion>) -> Result<String, ErrorMessage> {
+pub async fn new_question(AdminTokenInfo(user): AdminTokenInfo, Json(question): Json<NewQuestion>) -> Result<String, ErrorMessage> {
+    info!("Admin {} create question", user.uid);
     let question = Question::create(question).await?;
 
     Ok(question.id.to_string())
 }
 
-pub async fn modify_question(Json(question): Json<Question>) -> String {
+pub async fn modify_question(AdminTokenInfo(user): AdminTokenInfo, Json(question): Json<Question>) -> String {
+    info!("Admin {} modify question {}", user.uid, question.id);
     let result = question.to_entity().into_active_model().reset_all().update(&*crate::DATABASE).await.unwrap();
 
     result.id.to_string()
 }
 
-pub async fn swap_question(Json(question): Json<SwapQuestionRequest>) -> Result<String, ErrorMessage> {
+pub async fn swap_question(AdminTokenInfo(user): AdminTokenInfo, Json(question): Json<SwapQuestionRequest>) -> Result<String, ErrorMessage> {
+    info!("Admin {} swap question {} and {} of page {}", user.uid, question.from, question.to, question.page);
     Question::change_position(question.page, question.from, question.to).await?;
 
     Ok("".to_string())
